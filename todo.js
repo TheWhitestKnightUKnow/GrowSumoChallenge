56575c4650fa4e970dbc117f5c6348286a855cba
Rio Kivell <rjckivel@gmail.com>
2017-09-06 16:12:22 -0400

    Adding an id to the todos, for access in deletion and completion.

--- todo.js
+++ todo.js
@@ -1,5 +1,9 @@
+const uuidV4 = require('uuid/v4');
+
 module.exports = class Todo {
     constructor(title='') {
+        // The todo's id
+        this.id = uuidV4();
         // The title (and content) of the todo
         this.title = title;
         // Whether this todo has been completed or not
